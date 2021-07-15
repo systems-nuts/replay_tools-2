b71790843b08a468f9a3c4d6b64a30b0d1ced539
K1 <tong@kone.example.org>
2021-07-15 23:39:58 +0100

    clean

---
+++ rx_replay/cgroup_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NUM=$2
+BASE=10
+#Major 10, minor from 10 to $NUM-1
+TARGET=`expr $BASE + $NUM - 1`
+for i in $(seq $BASE 1 $TARGET)
+do
+	sudo mkdir -p /sys/fs/cgroup/net_cls/replay$i
+	sudo sh -c "echo 0x1000$i > /sys/fs/cgroup/net_cls/replay$i/net_cls.classid"
+	sudo tc qdisc add dev $1 root handle 10: htb
+	sudo tc class add dev $1 parent 10: classid 10:$i htb rate 10mbit
+	sudo tc filter add dev $1 parent 10:$i protocol ip prio 10 handle 1: cgroup
+done
+
---
+++ rx_replay/host_rx.py
@@ -0,0 +1,76 @@
+import csv
+import time
+import os
+import subprocess
+from itertools import islice
+vm_time=[]
+with open('rx.csv', newline='', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    for row in islice(reader, 1, None):
+        vm=[[0 for x in range(6)] for y in range(10)]
+        c=0
+        for i in range(19,len(row)-1):
+            if (i-19)%10==0:
+                vm[c][0]=row[i+2]
+                vm[c][1]=row[i+4]
+                vm[c][2]=row[i+5]
+                vm[c][3]=row[i+6]
+                vm[c][4]=row[i+7]
+                vm[c][5]=row[i+8]
+                c+=1
+        vm_time.append(vm)
+def network_rx_of_vm(i):
+    rx=[]
+    if i==0:
+        i=1
+    for vv in vm_time:
+        rx.append(vv[i-1][4])
+    return rx
+
+def network_change(i,major,minor):
+    if i == '':
+        return 
+    j=int(i)
+    j=int((j*27)/1024)
+    if j == 0:
+        j=1000
+    i = str(j)
+    cmd="sudo tc class change dev br1 parent 10: classid "+str(major)+":"+str(minor)+" htb rate "+ i+"kbit"
+    #print(cmd)
+    subprocess.Popen(cmd,shell=True,stdout=None)
+c=0
+
+
+def init():
+    number_of_machines=6
+    cgroup_num=10
+    port=5001
+    ip=81
+    for i in range(0,number_of_machines):
+        print(port)
+        port+=1
+        network_rx_cmd = "sudo iperf -s -u -p "+str(port)
+        subprocess.Popen(network_rx_cmd,shell=True,stdout=None)
+    time.sleep(3)
+    for i in range(0,number_of_machines):
+        ip+=1
+        cgroup_num+=1
+        network_tx_cmd = "sudo cgexec -g net_cls:replay"+str(cgroup_num)+" iperf -c 192.168.10"+str(ip)+"  -t 600 -u -b 100mb"
+        subprocess.Popen(network_tx_cmd,shell=True,stdout=None)
+
+rx=[]
+
+for i in range(1,7):
+    rx.append(network_rx_of_vm(i))
+print("Please remember sudo")
+init()
+major=10
+for i in range(0,len(rx[0])):
+    start=time.time()
+    minor=10
+    for j in range(0,len(rx)):
+        network_change(rx[j][i],major,minor+j)
+    end=time.time()
+    time.sleep(0.3-(end-start))
+    end=time.time() 
+
